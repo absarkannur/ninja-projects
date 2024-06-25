d290002fc4689621b6d0ca912292e9d4de3867c3
Muhammed Absar <121879746+absarkannur@users.noreply.github.com>
2024-06-25 12:14:35 +0530

    fist push

---
+++ al-kenz/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Al Kenz Pharmaceutical</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+    <link href="./styles/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/init.css" />
+    <link rel="stylesheet" href="./styles/site.css" />
+
+</head>
+<body>
+
+    <?php include_once('./inc/header.inc') ?>
+
+    <section class="ak-container ak-banner" style="background-image: url('assets/images/banner-1.png">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 flex-center">
+                    
+                    <div class="banner-content">
+                        <h4>Al Kenz Pharmaceuticals</h4>
+                        <h1>A Renowned Pharmaceutical Enterprise</h1>
+                        <p>Dedicated to advancing global healthcare. With a legacy spanning over two decades, we boast a distinguished team committed to excellence.</p>
+
+                        <div class="button-group">
+                            <div>
+                                <button class="ak-invr-btn p-l-10 center">
+                                    OUR PRODUCTS
+                                    <img src="./assets/icons/arrow.svg" alt="">
+                                </button>
+                            </div>
+                            <div>
+                                <button class="ak-dft-btn p-l-10 center">
+                                    OUR PRODUCTS
+                                    <img src="./assets/icons/arrow-w.svg" alt="">
+                                </button>
+                            </div>
+                        </div>
+                    
+                    </div>
+
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">&nbsp;</div>
+            </div>
+        </div>
+    </section>
+
+    <?php include_once('./inc/footer.inc') ?>
+
+    <script src="./scripts/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
