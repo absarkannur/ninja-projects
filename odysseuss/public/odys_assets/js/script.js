7e45140286688a6dfedb2019cfa6c77a79e7f12f
Muhammed Absar <121879746+absarkannur@users.noreply.github.com>
2024-11-20 11:13:24 +0530

    Grwoth Section Done

--- odysseuss/public/odys_assets/js/script.js
+++ odysseuss/public/odys_assets/js/script.js
@@ -14,22 +14,73 @@ jQuery(document).ready(function($) {
 
     function whyus_section() {
 
-        $('.heading').removeClass('visibility').addClass('animate__fadeInDown');
+        $('#js_whyus .heading').removeClass('visibility').addClass('animate__fadeInDown');
+
+        var li = [ 0, 1, 2, 3, 4, ];
+
+        li.map( ( item, i ) => {
+            setTimeout(function() {
+                $('.whyus-list .list').eq(item).removeClass('visibility').addClass('animate__fadeIn');
+            },(i*100));
+        });
+
+    }
+
+
+    // ---------------------
+
+
+    new ScrollMagic.Scene({
+        triggerElement: "#js_growth",
+    })
+    .on('start', function () {
+        growth_section();
+    })
+    .addTo( controller );
+
+    function growth_section() {
+
+
+        $('#js_growth .sub-heading').removeClass('visibility').addClass('animate__fadeInDown');
+        $('#js_growth .heading').removeClass('visibility').addClass('animate__fadeInDown');
 
         setTimeout(function(){
-            var li = [ 0, 1, 2, 3, 4, ];
+
+            var li = [ 0, 1, 2 ];
 
             li.map( ( item, i ) => {
                 setTimeout(function() {
-                    $('.whyus-list .list').eq(item).removeClass('visibility').addClass('animate__fadeIn');
+                    $('.growth-highlights .list').eq(item).removeClass('visibility').addClass('animate__fadeIn');
                 },(i*100));
             });
 
-        }, 800);
+        }, 500);
+
+        setTimeout(function(){
+            $('.growth_image_circle').removeClass('visibility').addClass('animate__rotateIn');
+        }, 500);
+
+    }
+
+    // ---------------------
+
+
+    new ScrollMagic.Scene({
+        triggerElement: "#js_webelieve",
+    })
+    .on('start', function () {
+        webelieve();
+    })
+    .addTo( controller );
+
+    function webelieve() {
+
+
+        $('#js_webelieve .sub-heading').removeClass('visibility').addClass('animate__fadeInDown');
+        $('#js_webelieve .heading').removeClass('visibility').addClass('animate__fadeInDown');
 
 
 
     }
-
 
 });
--- odysseuss/resources/views/livewire/home-page.blade.php
+++ odysseuss/resources/views/livewire/home-page.blade.php
@@ -30,10 +30,12 @@
                         <li class="list animate__animated visibility">
 
                             <div class="row">
-                                <div class="col-2">
-                                    <img src={{ asset('odys_assets/images/section-icons/whyus-1.png') }} alt="" srcset="">
+                                <div class="col-3 flex-center">
+                                    <span class="image-circle">
+                                        <img src={{ asset('odys_assets/images/section-icons/whyus-1.png') }} alt="" srcset="">
+                                    </span>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <span class="text blue">Effectively performed transformative, strategic, agile</span>
                                     <span class="text">innovation / renovation for incremental growth</span>
                                 </div>
@@ -43,10 +45,12 @@
                         <li class="list animate__animated visibility">
 
                             <div class="row">
-                                <div class="col-2">
-                                    <img src={{ asset('odys_assets/images/section-icons/whyus-2.png') }} alt="" srcset="">
+                                <div class="col-3 flex-center">
+                                    <span class="image-circle">
+                                        <img src={{ asset('odys_assets/images/section-icons/whyus-2.png') }} alt="" srcset="">
+                                    </span>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <span class="text blue">Boosted relevance & sales via global or local</span>
                                     <span class="text">brand building / rejuvenation</span>
                                 </div>
@@ -55,10 +59,12 @@
                         </li>
                         <li class="list animate__animated visibility">
                             <div class="row">
-                                <div class="col-2">
-                                    <img src={{ asset('odys_assets/images/section-icons/whyus-3.png') }} alt="" srcset="">
+                                <div class="col-3 flex-center">
+                                    <span class="image-circle">
+                                        <img src={{ asset('odys_assets/images/section-icons/whyus-3.png') }} alt="" srcset="">
+                                    </span>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <span class="text blue">Successfully done local, regional, European scale</span>
                                     <span class="text">business turnarounds from 20 to 800M $</span>
                                 </div>
@@ -69,10 +75,12 @@
                     <ul class="whyus-list">
                         <li class="list animate__animated visibility">
                             <div class="row">
-                                <div class="col-2">
-                                    <img src={{ asset('odys_assets/images/section-icons/whyus-4.png') }} alt="" srcset="">
+                                <div class="col-3 flex-center">
+                                    <span class="image-circle">
+                                        <img src={{ asset('odys_assets/images/section-icons/whyus-4.png') }} alt="" srcset="">
+                                    </span>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <span class="text blue">We’re all about results</span>
                                     <span class="text">and have made it happen before</span>
                                 </div>
@@ -80,10 +88,12 @@
                         </li>
                         <li class="list animate__animated visibility">
                             <div class="row">
-                                <div class="col-2">
-                                    <img src={{ asset('odys_assets/images/section-icons/whyus-5.png') }} alt="" srcset="">
+                                <div class="col-3 flex-center">
+                                    <span class="image-circle">
+                                        <img src={{ asset('odys_assets/images/section-icons/whyus-5.png') }} alt="" srcset="">
+                                    </span>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-9">
                                     <span class="text blue">We can activate a network</span>
                                     <span class="text">of high professionals task-purposed</span>
                                 </div>
@@ -99,7 +109,81 @@
         </div>
     </section>
 
-    <div style="height: 500px">
+    <section class="container-fluid growth-wrapper" id="js_growth">
+        <div class="container">
+
+            <div class="row">
+                <div class="col">
+                    <h1 class="sub-heading animate__animated visibility">OUR MODEL</h1>
+                    <h1 class="heading animate__animated visibility">The Growth Wheel</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <ul class="growth-highlights">
+                        <li class="list animate__animated visibility"><span>Holistic / Modular Approach</span></li>
+                        <li class="list animate__animated visibility"><span>Consumer centric approach</span></li>
+                        <li class="list animate__animated visibility"><span>Aiming to respect, evolve & grow <br/>brand equity</span></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+
+                    <div class="growth-wheel">
+                        <img class="growth_image_circle animate__animated visibility" src={{ asset('odys_assets/images/growth-wheel.svg') }} alt="">
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+
+    <section class="container-fluid webelieve-wrapper" id="js_webelieve">
+        <div class="container">
+
+            <div class="row">
+                <div class="col">
+                    <h1 class="sub-heading animate__animated visibility">OUR COMPASS IS THE CONSUMER</h1>
+                    <h1 class="heading animate__animated visibility">We Believe In</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+
+                    <ul>
+                        <li>
+                            <div class="imagethmb">
+                                <div class="imagethmb_inner">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+
+                        </li>
+                        <li>
+
+                        </li>
+                        <li>
+
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+
+
+    <div style="height: 500px;background-color: rgb(193, 193, 193);">
 
     </div>
 
