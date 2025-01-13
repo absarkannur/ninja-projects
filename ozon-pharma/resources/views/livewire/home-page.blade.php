d9c11794790ef519127c3374b61c6c48d60072e0
Muhammed Absar <121879746+absarkannur@users.noreply.github.com>
2025-01-13 11:08:27 +0530

    secend push

--- ozon-pharma/resources/views/livewire/home-page.blade.php
+++ ozon-pharma/resources/views/livewire/home-page.blade.php
@@ -22,7 +22,75 @@
                 </div>
             </div>
         </div>
+
+        <div style="height: 80px;">&nbsp;</div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                    <div class="banner-box-one">
+                        <h3 class="header">Our products</h3>
+                        <p class="content-text">We offer high quality products comparable to the best globally</p>
+                        <button class="oz-button-lightblue">Explore Products</button>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                    <div class="banner-box-two">
+                        <div class="box-1">
+                            aa
+                        </div>
+                        <div class="box-2">
+                            bb
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
+    <section class="facilities-wrapper">
+
+        <div class="container"> {{-- Header --}}
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <h2 class="heading">OUR FACILITIES</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="container"> {{-- Header --}}
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                    <h2 class="sub-heading">Reliable and Trusted Pharmaceutical Innovation</h2>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                    <div class="carousel-button-group fr">
+
+                        <button class="oz-button-default no-padding">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+                            </svg>
+                        </button>
+                        <button class="oz-button-default no-padding">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+                            </svg>
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col col-12">
+                    <span>Hello -------------</span>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
 
 </main>
