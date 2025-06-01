aed7f5fd0d16891bc3f7f214857312014be80a0f
Digital Venture <absar.design@gmail.com>
2025-06-01 13:09:38 +0530

    Update Voksan

--- voksan-1/assets/scripts/scripts.js
+++ voksan-1/assets/scripts/scripts.js
@@ -12,4 +12,24 @@ $(function () {
     $(".vertical_tabs_2 .tab-wrap").removeClass("active");
     $(".vertical_tabs_2 .tab-wrap").eq(index).addClass("active");
   });
+
+  // Home Page slider
+
+  var home_owl = $(".home-slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
+  });
+
+  $(".js_next").click(function () {
+    home_owl.trigger("next.owl.carousel");
+  });
+  $(".js_prev").click(function () {
+    home_owl.trigger("prev.owl.carousel");
+  });
 });
